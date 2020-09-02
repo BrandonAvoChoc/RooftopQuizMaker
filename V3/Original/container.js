a8f24e1a35e2788a9d017420063255f3746e229e
Brandon Bezuidenhout <Brandon@avochoc.com>
2020-09-02 10:19:46 +0200

    Code Refactor

---
+++ V3/Original/container.js
@@ -0,0 +1,7 @@
+function determineScreensize() {
+    if (screen.width <= 699) {
+        window.open("draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html", '_blank');
+    } else {
+        location.href = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+    }
+}
