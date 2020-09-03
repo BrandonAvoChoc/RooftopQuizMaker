378c03f2eb05505d22b690f4be3837183bdf3846
Brandon Bezuidenhout <Brandon@avochoc.com>
2020-09-03 10:00:19 +0200

    Implement Dropdown Selector

--- V3/Original/container.js
+++ V3/Original/container.js
@@ -1,7 +1,44 @@
+let language = 'english'
+
 function determineScreensize() {
-    if (screen.width <= 699) {
-        window.open("draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html", '_blank');
-    } else {
-        location.href = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+    let quizLink = '';
+    switch (language) {
+        case 'english':
+            quizLink = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+            break;
+        case 'عربى':
+            quizLink = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+            break;
+        case 'русский':
+            quizLink = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+            break;
+        case 'Español':
+            quizLink = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+            break;
+        case 'Français':
+            quizLink = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
+            break;
+        case 6:
+            quizLink = "draft%203/World%20Ozone%20Day%20Quiz%20(Published)/index.html";
     }
+
+    console.log(quizLink);
+
+    if (screen.width <= 699) {
+        window.open(quizLink, '_blank');
+    } else {
+        location.href = quizLink;
+    }
+}
+
+function submitLanguage(event) {
+    event.preventDefault();
+    console.log(language);
+    determineScreensize();
+}
+
+function setLanguage(event) {
+    event.preventDefault();
+    language = event.target.value;
+    console.log(event.target.value);
 }
